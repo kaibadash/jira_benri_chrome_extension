76bc81a6b72d433fa72ca7d4c072469ad7204b44
kaiba <kaibadash@gmail.com>
2018-09-10 02:32:16 +0900

    :+1: First version

--- app/scripts/options.ts
+++ app/scripts/options.ts
@@ -1,7 +1,32 @@
-chrome.storage.sync.get([Settings.KEY_TEAM_ID, Settings.KEY_TEMPLATE], (v: Settings) => {
-    this.settings = v;
-    if (callback == null) {
+import * as superagent from 'superagent';
+
+initialize();
+
+function initialize() {
+    document.getElementById("applyButton")!.addEventListener("click", loadSetting);
+    let text = document.getElementById("settingJsonUrl") as HTMLInputElement;
+    if (localStorage.getItem("url")) {
+        text.value = localStorage.getItem("url")!;
+    }
+}
+
+function loadSetting() {
+  // FIXME: 設定classにまとめたい
+  let text = document.getElementById("settingJsonUrl") as HTMLInputElement;
+  let url = text!.value;
+  superagent
+  .get(url!)
+  .end((err, res) => {
+    if (err) {
+        alert(err);
         return;
     }
-    callback(this);
-});
+    localStorage.setItem("url", url);
+    localStorage.setItem("teamId", res.body.teamId);
+    localStorage.setItem("issuePrefix", res.body.issuePrefix);
+    localStorage.setItem("pid", res.body.pid);
+    localStorage.setItem("issueType", res.body.issueType);
+    localStorage.setItem("summary", res.body.summary);
+    localStorage.setItem("description", res.body.description);
+  });
+}
