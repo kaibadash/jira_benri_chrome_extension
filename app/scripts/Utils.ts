37ef54c26f54731827617d11663d0206d0c52e9b
kaiba <kaibadash@gmail.com>
2018-09-19 21:57:49 +0900

    Add Utils

---
+++ app/scripts/Utils.ts
@@ -0,0 +1,8 @@
+export class Utils {
+    public static toQueryParam(hash: {[key: string] : string }): string {
+        let ret = [];
+        for (let d in hash)
+          ret.push(encodeURIComponent(d) + '=' + encodeURIComponent(hash[d]));
+        return ret.join('&');
+     }
+}
