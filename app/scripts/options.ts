0cca03a207d7d85690f16b3518f7c81ca560d696
kaiba <kaibadash@gmail.com>
2018-09-19 21:57:41 +0900

    Fix to set other params

--- app/scripts/options.ts
+++ app/scripts/options.ts
@@ -22,12 +22,11 @@ function loadSetting() {
         return;
     }
     localStorage.setItem("url", url);
-    localStorage.setItem("teamId", res.body.teamId);
-    localStorage.setItem("issuePrefix", res.body.issuePrefix);
-    localStorage.setItem("pid", res.body.pid);
-    localStorage.setItem("issueType", res.body.issueType);
-    localStorage.setItem("summary", res.body.summary);
-    localStorage.setItem("description", res.body.description);
+    console.log(Object.keys(res.body));
+    Object.keys(res.body).forEach((k : string) => {
+        console.log(k, res.body[k]);
+        localStorage.setItem(k, res.body[k]);
+    });
     window.close();
   });
 }
--- app/scripts/popup.ts
+++ app/scripts/popup.ts
@@ -1,18 +1,27 @@
-import 'chromereload/devonly';
-
+import "chromereload/devonly";
+import * as utils from "./Utils";
 initialize();
 
 function initialize() {
     document.getElementById("buttonAddNewIssue")!.addEventListener("click", addNewIssue);
     document.getElementById("buttonOpenIssue")!.addEventListener("click", openIssue);
+    if (localStorage.getItem("teamId") == null) {
+        window.open("options.html");
+    }
 }
 
 function addNewIssue() {
     let team = localStorage.getItem("teamId")!;
-    let description = encodeURIComponent(localStorage.getItem("description")!);
-    let pid = localStorage.getItem("pid")!;
-    let issueType = localStorage.getItem("issueType")!;
-    window.open(`https://${team}.atlassian.net/secure/CreateIssueDetails!init.jspa?pid=${pid}&issuetype=${issueType}&description=${description}`);
+    let params: {[key: string]: any} = {};
+    for (let key in localStorage){
+        console.log(key);
+        if (localStorage.getItem(key) == null) {
+            continue;
+        }
+        params[key] = localStorage.getItem(key)!;
+    }
+    
+    window.open(`https://${team}.atlassian.net/secure/CreateIssueDetails!init.jspa?${utils.Utils.toQueryParam(params)}`);
 }
 
 function openIssue() {
